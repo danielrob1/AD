44588f7ba8f97791146b10a6c2c2fab1d36da923
danielrob1 <168429589+danielrob1@users.noreply.github.com>
2024-10-24 10:37:28 +0200

    Sin cambios

--- Ficheros/src/sax/Ejercicio13Amp.java
+++ Ficheros/src/sax/Ejercicio13Amp.java
@@ -6,18 +6,10 @@ import javax.xml.parsers.SAXParserFactory;
 public class Ejercicio13Amp {
     public static void main(String[] args) {
         try {
-            // Crear la fábrica de parsers SAX
             SAXParserFactory factory = SAXParserFactory.newInstance();
-            
-            // Crear un nuevo parser SAX
             SAXParser saxParser = factory.newSAXParser();
-            
-            // Crear una instancia del manejador personalizado
             ManejadorSAX manejador = new ManejadorSAX();
-            
-            // Procesar el archivo XML
             saxParser.parse("Ejercicio13.xml", manejador);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
